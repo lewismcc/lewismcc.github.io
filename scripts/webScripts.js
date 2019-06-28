e17b1d98c861959459c4d46edfc756136b19ef68
lewismcc <lewismac23@hotmail.com>
2019-06-28 17:13:12 +0100

    slideshow (80%)

--- scripts/webScripts.js
+++ scripts/webScripts.js
@@ -62,7 +62,9 @@ function lastEntry(){
     $(".container").append(carDiv);
         
 };
-
+var i=0;
+var slideIndex = 1;
+showSlides(slideIndex)
 function plusSlides(n){
     showSlides(slideIndex += n);
     console.log("plus");
@@ -70,22 +72,20 @@ function plusSlides(n){
 function currentSlide(n){
     showSlides(slideIndex += n);
 }
-function showSlides(n){
+
+function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("dot");
-    if(n > slides.length){
-        slideIndex = 1;
-    }; 
-    if(n < 1){
-        slideIndex = slides.length;
-    };
-    for(i= 0; i < slideIndex.length; i++ ){
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
-    };
-    for(i = 0; i < dots.length; i++){
-        dots[i].className = dots[i].className.replace(" active", "");
-    };
-    slides[slideIndex -1 ].style.display="block";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = 
+        dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
-}
+  } 
