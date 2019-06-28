381126554bf466e548905e348e6b7e03137591e3
unknown <lewismac23@hotmail.com>
2019-06-28 09:52:21 +0100

    more slides

--- scripts/webScripts.js
+++ scripts/webScripts.js
@@ -68,3 +68,8 @@ function plusSlides(n){
 function currentSlide(n){
     showSlides(slideIndex += n);
 }
+function showSlides(n){
+    var i;
+    var slides = document.getElementsByClassName("mySlides")
+    var dots = document.getElementsByClassName("dot");
+}
