423c5b134e8e5c2505662c426bb68408c85afe19
unknown <lewismac23@hotmail.com>
2019-06-28 12:10:18 +0100

    sildework

--- scripts/webScripts.js
+++ scripts/webScripts.js
@@ -62,14 +62,30 @@ function lastEntry(){
     $(".container").append(carDiv);
         
 };
+
 function plusSlides(n){
     showSlides(slideIndex += n);
+    console.log("plus");
 }
 function currentSlide(n){
     showSlides(slideIndex += n);
 }
 function showSlides(n){
     var i;
-    var slides = document.getElementsByClassName("mySlides")
+    var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("dot");
+    if(n > slides.length){
+        slideIndex = 1;
+    }; 
+    if(n < 1){
+        slideIndex = slides.length;
+    };
+    for(i= 0; i < slideIndex.length; i++ ){
+        slides[i].style.display = "none";
+    };
+    for(i = 0; i < dots.length; i++){
+        dots[i].className = dots[i].className.replace(" active", "");
+    };
+    slides[slideIndex -1 ].style.display="block";
+    dots[slideIndex-1].className += " active";
 }
