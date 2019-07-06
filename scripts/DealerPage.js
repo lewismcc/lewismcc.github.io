7a751605dcda15f1fdf9b6bcf93993edc98bdcf2
lewismcc <lewismac23@hotmail.com>
2019-07-06 19:33:20 +0100

    need new api

--- scripts/DealerPage.js
+++ scripts/DealerPage.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
     url: 'https://api.mercedes-benz.com/dealer_tryout/v1',
     dataType: 'json',
     headers: {
-        'X-Auth-Token':'873fd943-4fb5-4120-b7f8-3913f4defde6'
+        'X-Auth-Token':'873fd943-4fb5-4120-b7f8-3913f4defde6',
     },
     success: function(rawData){
         console.log(rawData);
