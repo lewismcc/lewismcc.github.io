f6f1e054d5533678266b73fe0af0198cbf53a127
lewismcc <lewismac23@hotmail.com>
2019-07-13 22:48:12 +0100

    animation work started

--- scripts/LookToThePastScript.js
+++ scripts/LookToThePastScript.js
@@ -4,51 +4,65 @@ These functions allow the slide show to move to the next and previous slide thro
 with arrow buttons
 it displays the first image in the webpage as default
 */
-$(document).ready(function(){
-    
-    
-    $.ajax({
-      type: "get",
-      url: "./data/gullwing.csv",
-      datatype:"text",
-      success:function(rawImages){
-        outPutImages(rawImages)
-      }
-    })
-    function outPutImages(rawImages){
-      window.currentSlideIndex = 2;
-      window.carImages = $.csv.toObjects(rawImages)
-      carouselImages();
-    }  
-    });
-    function carouselImages(){
+$(document).ready(function () {
 
-      let content = "<img class='display-img' src='./images/gullwingImages/"+carImages[currentSlideIndex].image +"'>";
 
-      $("#carousel-image").html(content);
-
+  $.ajax({
+    type: "get",
+    url: "./data/gullwing.csv",
+    datatype: "text",
+    success: function (rawImages) {
+      outPutImages(rawImages)
     }
-    $("#previous-slide").click(function(){
-      currentSlideIndex--;
-      if(currentSlideIndex < 0){
-        currentSlideIndex = carImages.length -1;
-      }
-    
-      carouselImages();
-    })
-    $("#next-slide").click(function(){
-      currentSlideIndex++;
-      if(currentSlideIndex == carImages.length ){
-        currentSlideIndex =0;
-      }
-      else if(currentSlideIndex > carImages.length -1){
-        currentSlideIndex--;
-      }
-      carouselImages();
-    })
+  })
 
+  function outPutImages(rawImages) {
+    window.currentSlideIndex = 2;
+    window.carImages = $.csv.toObjects(rawImages)
+    carouselImages();
+  }
+  $("#animation-image").click(function () {
+    console.log("running animation");
+    var outputVideo=
+    "<div class='video-box'>"+
+    "<video width='500px' height='350px' controls>"+
+    "<source src='history-clip.mp4' type='video/mp4'>"+
+    +"</div>";
+    $(".amination-image").append(outputVideo);   
+  })
 
+});
 
-    //#region 
-    
-  
+function carouselImages() {
+
+  let content = "<img class='display-img' src='./images/gullwingImages/" + carImages[currentSlideIndex].image + "'>";
+
+  $("#carousel-image").html(content);
+
+}
+$("#previous-slide").click(function () {
+  currentSlideIndex--;
+  if (currentSlideIndex < 0) {
+    currentSlideIndex = carImages.length - 1;
+  }
+
+  carouselImages();
+})
+$("#next-slide").click(function () {
+  currentSlideIndex++;
+  if (currentSlideIndex == carImages.length) {
+    currentSlideIndex = 0;
+  } else if (currentSlideIndex > carImages.length - 1) {
+    currentSlideIndex--;
+  }
+  carouselImages();
+})
+// $("#animation-image").click(function () {
+//   console.log("running animation");
+//   var outputVideo=
+//   "<div class='video-box'>"+
+//   "<video width='500px' height='350px' controls>"+
+//   "<source src='history-clip.mp4' type='video/mp4'>"+
+//   +"</div>";
+//  // $(".amination-image")
+// })
