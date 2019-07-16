2bd95b8bc10a6f21ae1a27756e054ca32900e838
Piotr Gospodarczyk <fedpy@protonmail.com>
2019-07-16 11:12:55 +0100

    Geolocation Web API implemented

--- scripts/DealerPage.js
+++ scripts/DealerPage.js
@@ -1,7 +1,45 @@
 $(document).ready(function($) {
 
-	// TODO: Get the user geolocation
-	// TODO: What if the user has geolocation disabled?
+  /*
+    Check if the browser supports
+    the Geolocation Web API
+  */
+  if (!navigator.geolocation) {
+
+    console.log("Geolocation is not supported by your browser");
+
+  } else {
+
+    // Get user current position
+    navigator.geolocation.getCurrentPosition(GeolocationSuccess, GeolocationError);
+
+  }
+
+  /*
+    Geolocation Success
+    This function handles the response
+    from Get Current Position function
+  */
+  function GeolocationSuccess(position) {
+
+    var lat = position.coords.latitude;
+    var lon = position.coords.longitude;
+
+    console.log("Lat: " + lat + "Long: " + lon);
+
+  }
+
+  /*
+    Geolocation Error
+    This function handles the error
+    from Get Current Position function
+  */
+  function GeolocationError(error) {
+
+    // TODO: What if the user has geolocation disabled?
+    console.log("Geolocation failed: " + error.message);
+
+  }
 
 	// TODO: Make jQuery AJAX request to the api.php file
 	// TODO: Feed the jQuery AJAX request with GPS coordinates
