7f48484840b7252149de3f3508d98debfa71f054
lewismcc <lewismac23@hotmail.com>
2019-07-20 22:52:36 +0100

    fiddle

--- scripts/DealerPage.js
+++ scripts/DealerPage.js
@@ -54,6 +54,9 @@ $(document).ready(function($) {
       url: "./data/dealers.json",
       type: "GET",
       dataType: "json",
+      success:function(garageInfo){
+        displayInfo(garageInfo)
+      }
       // TODO: Change to production version
       // data: {
       //   "latitude": lat,
@@ -73,4 +76,37 @@ $(document).ready(function($) {
     });
   };
 
+  function displayInfo(garageInfo) {
+    var weekdays = Object.entries(garageInfo.dealers[0].openingHours[0].weekdays);;
+    var openTimes = "<br>";
+    var link1 = garageInfo.dealers[0].communicationChannels.website;
+    var tempLink = link1.slice(0,4);
+    var tempLink2 = link1.slice(4);
+    link = tempLink +"s"+tempLink2;
+    console.log(link);
+
+
+    for (i = 0; i < weekdays.length; i++) {
+      openTimes = openTimes +
+        "<dd>" +
+        weekdays[i][0] + " : " +
+        weekdays[i][1].status +
+        "</dd>"
+    };
+    //console.log(garageInfo.dealers[0].communicationChannels.facebook);
+    //link = garageInfo.dealers[0].communicationChannels.website
+    var outputContent =
+      "<div class= 'garage'>" +
+      "<h1> Garage Name: " + garageInfo.dealers[0].legalName + "</h1>" +
+      "<div class='left'>" +
+      "<iframe src='" + link + "'" +
+      "width='650' height ='400' frameborder='0' style='border:0' class='dealerWebsite' allowfullscreen></iframe>" +
+      "<div class='right'>" +
+      "<dl>" +
+      "<dt>" + garageInfo.dealers[0].address.street + " " + garageInfo.dealers[0].address.city +
+      "</dt>" +
+      openTimes +
+      "</dl>";
+    $("#output").append(outputContent);
+  }
 });
